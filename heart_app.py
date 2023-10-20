28d87582856c7a3fe6de70e6f7c4f37574245665
9650ro85hit <136914545+9650ro85hit@users.noreply.github.com>
2023-10-20 22:43:42 +0530

    Update heart_app.py

--- heart_app.py
+++ heart_app.py
@@ -1,35 +1,27 @@
-import pickle
+
 import streamlit as st
-import numpy as np
 import sklearn
-
-st.header('Heart Disease Predictor', divider='rainbow')
-
-with open('heart_disease_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+import pickle
+import numpy as np
+import pandas as pd
 
 
+st.header("Heart disease predictor")
+new_model = pickle.load(open('heart_disease_model.pkl','rb'))
 
-st.title('Heart Disease Predictor')
-st.write(model)
-input_data = (20, 0, 0, 140, 268, 0, 0, 160, 0, 3.6, 0, 2, 2)
 
-st.write(model.predict())
-# Create input elements (e.g., sliders, text input fields) for users to enter data
-age = st.slider('Age', 20, 100, 50)
-# Add input elements for other features here...
+input_data = (62,0,0,140,268,0,0,160,0,3.6,0,2,2)
 
-# Make predictions when a user clicks a button
-if st.button('Predict'):
-    input_data = (20, 0, 0, 140, 268, 0, 0, 160, 0, 3.6, 0, 2, 2)
-    # input_data = (.2, 0, 0, .140, .268, 0, 0, .160, 0, .36, 0, .2, .2)
-    input_data_as_numpy_array = np.asarray(input_data)
-    
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    st.write(input_data_reshaped)
-    prediction = model.predict(input_data_reshaped)
+# change the input data to a numpy array
+input_data_as_numpy_array= np.asarray(input_data)
 
-    # if prediction[0] == 0:
-    #     st.write('The person does not have heart disease.')
-    # else:
-    #     st.write('The person has heart disease.')
+# reshape the numpy array as we are predicting for only on instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = new_model.predict(input_data_reshaped)
+st.write(prediction)
+
+if (prediction[0]== 0):
+  st.write('The Person does not have a Heart Disease')
+else:
+  st.write('The Person has Heart Disease')
